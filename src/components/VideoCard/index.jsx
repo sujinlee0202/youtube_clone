766b6c2acb1391c77b2cbf44afaa234768f668e3
titch <sujinlee0202@gmail.com>
2023-03-22 16:14:56 +0900

    채널 이미지 추가

--- src/components/VideoCard/index.jsx
+++ src/components/VideoCard/index.jsx
@@ -1,32 +1,34 @@
 import { useQuery } from '@tanstack/react-query'
-import React from 'react'
+import React, { useContext } from 'react'
 import { useNavigate } from 'react-router'
-import { fetchChannel } from '../../api/fetchChannel'
 import { getDate } from '../../api/getDate'
+import { YoutubeAPIContext } from '../../context/YoutubeAPIContext'
 
 const VideoCard = ({videos, type}) => {
+  const youtubeContext = useContext(YoutubeAPIContext)
   const {thumbnails, title, channelId, channelTitle, publishedAt, description} = videos.snippet
-  const {data: channel, isLoading, error} = useQuery(['channel', channelId], fetchChannel, {
+  const {data: channel, isLoading, error} = useQuery(['channel', channelId], 
+  () => youtubeContext.channel(channelId), {
     staleTime: 1000 * 60 * 5
   })
   const navigate = useNavigate()
 
   const onClickVideoDetail = () => {
-    navigate(`/watch/${videos.id.videoId ? videos.id.videoId : videos.id}`)
+    navigate(`/watch?v=${videos.id.videoId ? videos.id.videoId : videos.id}`)
   }
   
   if(isLoading) return <div>loading...</div>
   if(error) return <div>{error.message}</div>
-  
+
+  const url = channel.thumbnails.default.url
+
   return (
     <>
       {type === 'main' && (
           <li className='w-full' onClick={onClickVideoDetail}>
             <img src={thumbnails.medium.url} alt='video thumbnail' className='w-full mb-2'></img>
             <div className='flex w-full gap-3'>
-              <div className='w-10 h-10'>
-                <img src={channel.snippet.thumbnails.default.url} alt='channel' className='w-10 h-10 rounded-full'></img>
-              </div>
+              {url && <img src={url} alt='channel' className='w-10 h-10 rounded-full'></img>}
               <div className='w-full'>
                 <h2 className='w-full font-bold mb-2 line-clamp-2'>{title}</h2>
                 <p className='text-sm text-gray-500'>{channelTitle}</p>
@@ -43,9 +45,7 @@ const VideoCard = ({videos, type}) => {
               <h2 className='w-full font-bold mb-1 line-clamp-2'>{title}</h2>
               <p className='text-sm text-gray-500'>{getDate(publishedAt)}</p>
               <div className='flex items-center gap-3 my-4'>
-                <div className='w-10 h-10'>
-                  <img src={channel.snippet.thumbnails.default.url} alt='channel' className='w-10 h-10 rounded-full'></img>
-                </div>
+                {url && <img src={url} alt='channel' className='w-10 h-10 rounded-full'></img>}
                 <p className='text-sm text-gray-500'>{channelTitle}</p>
               </div>
               <p className='text-sm text-gray-500 line-clamp-2'>{description}</p>
