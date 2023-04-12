8bfad40a7e45f10f25a6275f0a02e576d25c598a
titch <sujinlee0202@gmail.com>
2023-04-12 20:29:22 +0900

    실제 Youtube API 사용으로 변경, README 수정

--- src/context/YoutubeAPIContext.jsx
+++ src/context/YoutubeAPIContext.jsx
@@ -8,8 +8,8 @@ export const YoutubeAPIContext = createContext()
  * 유튜브 api를 local, real로 쉽게 바꿀 수 있게 작성한 context
  */
 export const YoutubeAPIProvider = ({children}) => {
-  const youtube = new LocalYoutube()
-  // const youtube = new RealYoutube()
+  // const youtube = new LocalYoutube()
+  const youtube = new RealYoutube()
 
   return (
     <YoutubeAPIContext.Provider value={youtube}>
